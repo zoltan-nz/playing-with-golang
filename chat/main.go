1b0ea4767157eb8f4090405b9c3b03a8e93ca5b4
Zoltan <zdebre@gmail.com>
2017-10-26 22:07:00 +1300

    Add basic for loop

--- chat/main.go
+++ chat/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+
+	i := 0
+
+	for {
+		if i>= 5 {
+			break
+		}
+
+		fmt.Println(i)
+		i++
+	}
 }
