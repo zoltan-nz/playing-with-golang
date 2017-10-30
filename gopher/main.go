9ccaed9a775f6e22762c4c3b25f3c91387c7c066
Zoltan <zdebre@gmail.com>
2017-10-30 22:07:00 +1300

    Using references and pointers

--- gopher/main.go
+++ gopher/main.go
@@ -5,6 +5,7 @@ import "fmt"
 type gopher struct {
 	name string
 	age  int
+	isAdult bool
 }
 
 func (g gopher) jump() string {
@@ -14,10 +15,16 @@ func (g gopher) jump() string {
 	return g.name + " can still jump"
 }
 
+func validateAge(g *gopher) {
+	g.isAdult = g.age >= 21
+}
+
 func main() {
-	gopher1 := gopher{name: "Joe", age: 37}
+	gopher1 := &gopher{name: "Joe", age: 37}
 	gopher2 := gopher{name: "Bill", age: 70}
 
-	fmt.Println(gopher1.jump())
-	fmt.Println(gopher2.jump())
+	validateAge(gopher1)
+
+	fmt.Println(gopher1)
+	fmt.Println(gopher2)
 }
