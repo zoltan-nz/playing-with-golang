aba6ce93c08e7a941ba8b2f856345745bfb00b81
Zoltan <zdebre@gmail.com>
2017-11-04 22:07:00 +1300

    Using sync.WaitGroup for parallelism

--- concurrent/main.go
+++ concurrent/main.go
@@ -1,14 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"sync"
+)
 
 func main() {
 	names := []string{"First", "Second", "Third"}
+	var wg sync.WaitGroup
+	wg.Add(len(names))
 	for _, name := range names {
-		printName(name)
+		go printName(name, &wg)
 	}
+	wg.Wait()
 }
 
-func printName(n string) {
+func printName(n string, wg *sync.WaitGroup) {
+	result := 0.0
+	for i := 0; i < 1000000000; i++ {
+		result += math.Pi * math.Sin(float64(len(n)))
+	}
 	fmt.Println("Name: ", n)
+	wg.Done()
 }
